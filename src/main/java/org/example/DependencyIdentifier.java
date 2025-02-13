2fb5445bcfdf24dd4ca6801347bc97f0a6215dd8
Adrian <supernavigip@mail.ru>
2025-02-13 10:24:30 +0300

    feat: implement dependency identifier

---
+++ src/main/java/org/example/DependencyIdentifier.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public final class DependencyIdentifier {
+    private static final Pattern DEPENDENCY_PATTERN = Pattern.compile("require\\S+'(.*?)'");
+
+    public Map<Path, List<Path>> identifyDependencies(List<Path> textFilePaths){
+
+        Map<Path, List<Path>> dependencies = new HashMap<>();
+
+        for (Path textFilePath: textFilePaths){
+            List<Path> requiredFiles = new ArrayList<>();
+            List<String> lines = null;
+            try {
+                lines = Files.readAllLines(textFilePath);
+            } catch (IOException e) {
+                System.out.println("Возникла ошибка при чтении файла: " + e.getMessage());
+            }
+            for (String line: lines){
+                Matcher matcher = DEPENDENCY_PATTERN.matcher(line);
+                if (matcher.find()){
+                    String requiredPathStr = matcher.group(1);
+                    Path requiredPath = Paths.get(requiredPathStr);
+                    requiredFiles.add(requiredPath);
+                }
+            }
+            dependencies.put(textFilePath, requiredFiles);
+        }
+        return dependencies;
+    }
+}
